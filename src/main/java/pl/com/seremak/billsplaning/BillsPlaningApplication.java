30aefc80db5e11372d90c5d834ecb8a174a6f509
lseremak <llseremak@gmail.com>
2022-10-29 17:28:33 +0200

    first commit

---
+++ src/main/java/pl/com/seremak/billsplaning/BillsPlaningApplication.java
@@ -0,0 +1,13 @@
+package pl.com.seremak.billsplaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsPlaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsPlaningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/seremak/billsplaning/BillsPlaningApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.seremak.billsplaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsPlaningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
