e6c864f68fc8120c6c1f45b41bed34d4445c535e
lseremak <llseremak@gmail.com>
2022-11-07 14:46:30 +0100

    add BillActionMessage

--- src/main/java/pl/com/seremak/billsplaning/messageQueue/MessageListener.java
+++ src/main/java/pl/com/seremak/billsplaning/messageQueue/MessageListener.java
@@ -4,9 +4,12 @@ package pl.com.seremak.billsplaning.messageQueue;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+import pl.com.seremak.billsplaning.messageQueue.queueDto.BillActionMessage;
 import pl.com.seremak.billsplaning.service.CategoryService;
 
+import static pl.com.seremak.billsplaning.config.RabbitMQConfig.BILL_ACTION_MESSAGE;
 import static pl.com.seremak.billsplaning.config.RabbitMQConfig.USER_CREATION_QUEUE;
 
 @Slf4j
@@ -18,8 +21,14 @@ public class MessageListener {
 
 
     @RabbitListener(queues = USER_CREATION_QUEUE)
-    public void listenUserCreationQueue(final String username) {
+    public void receiveUserCreationMessage(final String username) {
         log.info("User creation message received. Username={}", username);
         categoryService.createStandardCategoriesForUserIfNotExists(username);
+    }
+
+    @RabbitListener(queues = BILL_ACTION_MESSAGE)
+    public static void receiveBillActionMessage(final Message<BillActionMessage> message) {
+        final BillActionMessage billActionMessage = message.getPayload();
+        log.info("Bill action message received. Username={}", billActionMessage);
     }
 }
