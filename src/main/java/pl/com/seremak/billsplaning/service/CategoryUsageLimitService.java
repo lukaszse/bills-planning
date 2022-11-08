8faac093afb3978c7ebd9e3f8fd9c2f34ba0af1b
lseremak <llseremak@gmail.com>
2022-11-08 20:18:55 +0100

    add total usage

--- src/main/java/pl/com/seremak/billsplaning/service/CategoryUsageLimitService.java
+++ src/main/java/pl/com/seremak/billsplaning/service/CategoryUsageLimitService.java
@@ -2,12 +2,14 @@ package pl.com.seremak.billsplaning.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 import pl.com.seremak.billsplaning.dto.TransactionDto;
 import pl.com.seremak.billsplaning.model.Category;
 import pl.com.seremak.billsplaning.model.CategoryUsageLimit;
 import pl.com.seremak.billsplaning.repository.CategoryUsageLimitRepository;
 import pl.com.seremak.billsplaning.repository.CategoryUsageLimitSearchRepository;
+import pl.com.seremak.billsplaning.utils.ListUtils;
 import pl.com.seremak.billsplaning.utils.VersionedEntityUtils;
 import reactor.core.publisher.Mono;
 
@@ -15,6 +17,7 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.YearMonth;
 import java.util.List;
+import java.util.Optional;
 
 import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 import static pl.com.seremak.billsplaning.converter.CategoryUsageLimitConverter.categoryUsageLimitOf;
@@ -34,7 +37,8 @@ public class CategoryUsageLimitService {
     public Mono<List<CategoryUsageLimit>> findAllCategoryUsageLimits(final String username, final String yearMonth) {
         final String yearMonthToSearch = defaultIfNull(yearMonth, toYearMonthString(Instant.now()).orElseThrow());
         return categoryUsageLimitRepository.findByUsernameAndYearMonth(username, yearMonthToSearch)
-                .collectList();
+                .collectList()
+                .map(CategoryUsageLimitService::addTotalUsageLimit);
     }
 
     public Mono<CategoryUsageLimit> updateCategoryUsageLimit(final TransactionDto transactionDto) {
@@ -54,6 +58,11 @@ public class CategoryUsageLimitService {
                 .flatMap(categoryUsageLimitRepository::save);
     }
 
+    private Mono<BigDecimal> getCategoryLimit(final String username, final String categoryName) {
+        return categoryService.findCategory(username, categoryName)
+                .map(Category::getLimit);
+    }
+
     private static CategoryUsageLimit updateCategoryUsageLimit(final CategoryUsageLimit categoryUsageLimit,
                                                                final TransactionDto transactionDto) {
         final BigDecimal updatedLimitUsage = updateBalance(categoryUsageLimit.getUsage(), transactionDto);
@@ -61,8 +70,22 @@ public class CategoryUsageLimitService {
         return categoryUsageLimit;
     }
 
-    private Mono<BigDecimal> getCategoryLimit(final String username, final String categoryName) {
-        return categoryService.findCategory(username, categoryName)
-                .map(Category::getLimit);
+    private static List<CategoryUsageLimit> addTotalUsageLimit(final List<CategoryUsageLimit> categoryUsageLimits) {
+        final Optional<Pair<BigDecimal, BigDecimal>> totalUsageAndLimitOpt = categoryUsageLimits.stream()
+                .map(categoryUsageLimit -> Pair.of(categoryUsageLimit.getUsage(), categoryUsageLimit.getLimit()))
+                .reduce((usageAndLimit1, usageAndLimit2) ->
+                        Pair.of(usageAndLimit1.getFirst().add(usageAndLimit2.getFirst()), usageAndLimit1.getSecond().add(usageAndLimit2.getSecond())));
+        final Optional<CategoryUsageLimit> totalOpt = categoryUsageLimits.stream().findFirst()
+                .flatMap(category -> totalUsageAndLimitOpt
+                        .map(totalUsageAndLimit -> CategoryUsageLimit.builder()
+                                .username(category.getUsername())
+                                .categoryName("total")
+                                .usage(totalUsageAndLimit.getFirst())
+                                .limit(totalUsageAndLimit.getSecond())
+                                .yearMonth(category.getYearMonth())
+                                .build()));
+        return totalOpt
+                .map(total -> ListUtils.addAndReturn(categoryUsageLimits, total))
+                .orElse(categoryUsageLimits);
     }
 }
